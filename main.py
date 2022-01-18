192ca99f42102f0dabc90ea67662714ab2a4f4d8
jacallmeister <96734712+jacallmeister@users.noreply.github.com>
2022-01-18 21:27:17 +0200

    add code to files

---
+++ main.py
@@ -0,0 +1,84 @@
+a = 12
+b = 23
+c = a + b
+# c=a+b
+
+print(c)
+
+# ctr + d
+
+# print command
+print('hello world!')
+
+# переменные не должны начинатся с чисел
+# 2a = 4  +-=() (symbol)
+# учет регистра , переменные А и а разные
+
+# a != A - регистрозависимые
+value = 1
+# prinT(Value)
+
+
+first = 1
+second = 35
+sum_of_int = first + second
+
+student_1_mark = 12
+student_2_mark = 11
+average_mark = (student_1_mark + student_2_mark) #/ comment
+
+#int целые числа
+#float числа с запятой
+#comlex
+#str
+#bool
+
+#list
+#tuple
+#set
+#dict
+
+
+# int, float
+#first = 12
+#second = 12.0
+#print(type(first))
+#print(type(second))
+
+#c = 12 + 5 + 33 * 2 - 4.0
+#c = 10 / 5
+#c = 2 + .0
+#print(c)
+#print(type(c))
+
+#c = 0.1 + 0.2
+#c = c * 100
+#c = 2 ** 5  # возведение в степень
+#c = 0
+#c += 1 # c = c + 1
+
+#c = '23'
+
+#print(c)
+#print(type(c))
+
+#c = 4e-17   # 4 * (10 ** 17)
+#print(c)
+#print(type(c))
+
+
+#a = 2.3
+#a = int(a)
+#print(a)
+
+#a = '2.3'
+#a = float(a)
+
+#print(a)
+
+asd = 8 
+
+
+
+
+
